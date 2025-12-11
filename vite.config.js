029417a4df7b5524bd2041528e5f3e89fcd4799c
Mateo Estrada <mateoestrada1403@gmail.com>
2025-12-11 14:59:45 -0300

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/integration-builder-v1/', // Configura la ruta base para GitHub Pages
 })
